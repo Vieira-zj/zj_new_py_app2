bf75be0b3c03a6f7206ee3659b4551ee4f4cdda5
Zheng Jin <zivieira@163.com>
2026-04-13 11:49:20 +0800

    add download file util.

--- py-demo/utils/http_utils.py
+++ py-demo/utils/http_utils.py
@@ -1,2 +1,24 @@
+from pathlib import Path
+
+import requests
+from requests.models import Response
+
+
+def download_file(url: str, saved_fpath: str) -> None:
+    out = Path(saved_fpath)
+    if out.exists():
+        size_kb = out.stat().st_size / 1e3
+        print(f"{out}: {size_kb:.1f} KB (cached)")
+        return
+
+    resp: Response = requests.get(url, timeout=60)
+    resp.raise_for_status()
+
+    if resp.content:
+        out.write_bytes(bytes(resp.content))
+        size_kb = out.stat().st_size / 1e3
+        print(f"{out}: {size_kb:.1f} KB")
+
+
 if __name__ == "__main__":
     pass
